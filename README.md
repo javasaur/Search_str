36ec9fb996bcd167eeb8d98c3537ca1e589a1288
root <root@localhost.localdomain>
2018-03-06 14:09:32 +0200

    Added progress-bar, updated README.md, slight changes of formatting and naming

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# search
-Performs a search of string TEXT in current working directory and it's subfolders. 
-Call the script with no arguments to view the instructrions: 
+#Search
+A small routine to search a string in all files with specified extension in the current working directory and it's subfolders.
+
+#Getting started
+To see the usage instructions run
 ```
 nodejs search.js
 ```
 
+#Prerequisites
+You might install the **progress-bar** package to include a progress-bar in your search output
+```
+npm install progress-bar
+```
+This is optional and the script can execute without it
+
+#Installing 
+git clone https://github.com/javasaur/search.git
--- search.js
+++ search.js
@@ -1,16 +1,37 @@
+// Declare/define global variables
 const path = require('path');
 const fs = require('fs');
-const curDir = process.cwd();
-const flags = process.argv.slice(4);
+var curDir = process.cwd();
+var flags = process.argv.slice(4);
+var results = [];
+var strCaseFlag, extCaseFlag, countFlag; // flags
+var bar; // progress-bar
 
 // If the input is invalid, provide instructions
 if(process.argv.length < 4) {
 	showInstructions();
 }  else {
 	// Get the arguments and pass them to search function
-	const extension = process.argv[2];
-	const toSearch = process.argv[3];
-	search(curDir, extension, toSearch);
+	var extension = process.argv[2];
+	var toSearch = process.argv[3];
+	// Display progress-bar if dependency found
+	try {
+		bar = require('progress-bar').create(process.stdout);
+	} catch(err) {
+		console.log('No progress-bar module found; Omitting the progress-bar output');
+	}
+	results = results.concat(search(curDir, extension, toSearch));
+
+	if(results.length != 0) {
+		console.log('Files found: ' + results.length);
+		// Slight formatting in case 'count' flag is applied
+		results.forEach(match => { 
+			countFlag ? console.log(String(match.count).padStart(5) + ' | ' + match.path) : 
+			console.log(match.path); 
+		});
+	} else {
+		console.log('No matches found');
+	}
 }
 
 /**
@@ -21,7 +42,7 @@ if(process.argv.length < 4) {
  * @returns {Integer} Number of matches.
  */
 function findStr(file, str, strCase) {
-	/* No-encoding call returns an object of buffer, while calling with encoding argument
+	/* No-encoding call returns an object of raw buffer, while calling with encoding argument
 	   results in a string, thus allowing manipulation with the output */
 	var data = fs.readFileSync(file, 'utf8');
 	var regExp = strCase ? new RegExp(str, "g") : new RegExp(str, "gi");
@@ -30,35 +51,41 @@ function findStr(file, str, strCase) {
 
 /**
  * Performs a search of the string in the files with given extension, 
- * in the given directory and all it's subfolders.
+ * in the given directory and all it's subfolders. Fills the provided array
+ * with the found matches.
  * @param {String} dir The directory.
  * @param {String} ext The file extension.
  * @param {String} str String to search.
- * @returns void Only std output.
+ * @returns {Object[]} matches Array of matching objects
  */
 function search(dir, ext, str) {
 	// Define flags
-	const strCase = flags.includes('-strc') || flags.includes('-strcase');
-	const extCase = flags.includes('-extc') || flags.includes('-extrcase');
-	const countFlag = flags.includes('-count') || flags.includes('-c');
+	strCaseFlag = flags.includes('-strc') || flags.includes('-strcase');
+	extCaseFlag = flags.includes('-extc') || flags.includes('-extcase');
+	countFlag = flags.includes('-count') || flags.includes('-c');
 
 	console.log(`Starting search; Directory: ${dir} and subfolders;`);
-	console.log(`Extension: .${ext} files, case-sensitive: ${extCase}; String: ${str}, case-sensitive: ${strCase}`);
+	console.log(`Extension: .${ext}, case-sensitive: ${extCaseFlag}; String: ${str}, case-sensitive: ${strCaseFlag}\n`);
 
-	
-
-	var files = traverse(dir, ext, extCase);
+	// Get an array of matching files
+	var files = traverse(dir, ext, extCaseFlag);
 	
 	// Check if we have at least 1 file to search through
 	if(files.length == 0) {
 		console.log(`No ${ext} files found`);
-		return;
+		return [];
 	}
 
-	// Filter the matching files
+	// Fill the array with matching files
 	var matches = [];
+	var upd = 1;
 	files.forEach(file => {
-		var found = findStr(file, str, strCase); 
+		// Update progress-bar if included
+		if(bar != null) {
+			bar.update(upd * (1 / files.length))
+			upd++;
+		}
+		var found = findStr(file, str, strCaseFlag); 
 		if(found > 0) {
 			matches.push({
 				path: file,
@@ -66,16 +93,7 @@ function search(dir, ext, str) {
 			});
 		}
 	});
-
-	// Display the output
-	if(matches.length == 0) {
-		console.log('No matches found');
-	} else {
-		console.log('Matches found: ' + matches.length);
-		countFlag ? 
-		matches.forEach(match => console.log(String(match.count).padStart(5, " ") + ' | ' + match.path)) :
-		matches.forEach(match => console.log(match.path));
-	}
+	return matches;
 }
 
 /**
@@ -101,14 +119,18 @@ function showInstructions() {
  * @returns {String[]} An array of matching files or an empty array.
  */
 function traverse(dir, ext, extCase) {
-	var files = fs.readdirSync(dir).reduce((files, file) => 
-				fs.statSync(path.join(dir, file)).isDirectory() ?
-				files.concat(traverse(path.join(dir, file), ext)) :
-				files.concat(path.join(dir, file)),
-				[]).filter(file => extCase ?
-				path.extname(file) === '.' + ext :
-				path.extname(file).toUpperCase() === '.' + ext.toUpperCase()); 
-	return files;
+	try {
+		var files = fs.readdirSync(dir).reduce((files, file) => 
+					fs.statSync(path.join(dir, file)).isDirectory() ?
+					files.concat(traverse(path.join(dir, file), ext)) :
+					files.concat(path.join(dir, file)),
+					[]).filter(file => extCase ?
+					path.extname(file) === '.' + ext :
+					path.extname(file).toUpperCase() === '.' + ext.toUpperCase()); 
+	} catch(err) {
+		console.log(`Error opening ${dir}`);
+	}
+	return files || [];
 }
 
 
